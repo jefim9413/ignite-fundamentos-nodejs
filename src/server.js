e2d7f006deb82aa7cc92c5aea95f53f6c160b3a5
Jefferson Marques <jefferson.jsm3444@gmail.com>
2023-01-23 16:18:13 -0300

    aula 1 e 2 concluida

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-const http = require('http');
+import http from 'node:http';
+
+
+const server = http.createServer((request, response) => {
+    return response.end('hello rld');   
+})
+
+server.listen(3333)

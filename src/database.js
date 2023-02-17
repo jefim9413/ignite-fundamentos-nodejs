ebe25c9082b1b68cb69302a1e3d4ebf3727c4a7d
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-02-17 16:11:08 -0300

    trabalhando persistencia

--- src/database.js
+++ src/database.js
@@ -1,7 +1,20 @@
 
+import fs from 'node:fs/promises'
+const databasepath = new URL('../db.json', import.meta.url)
 
 export class Database {
     #database = {}
+    constructor(){
+        fs.readFile(databasepath, "utf8").then(data =>{
+            this.#database = JSON.parse(data)
+        })
+        .catch(()=> {
+            this.#parsist()
+        })
+    }
+    #parsist(){
+        fs.writeFile('db.json',JSON.stringify(this.#database))
+    }
 
     select(table){
         const data = this.#database[table]??[]
@@ -15,6 +28,7 @@ export class Database {
         }else {
             this.#database[table] = [data]
         }
+        this.#parsist()
 
         return data
     }

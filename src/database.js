6cad4ff1a7a6687e28c3c7717b4d38cfed493757
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-02-22 13:56:34 -0300

    adiconando funções de CRUD

--- src/database.js
+++ src/database.js
@@ -1,35 +1,48 @@
-
 import fs from 'node:fs/promises'
-const databasepath = new URL('../db.json', import.meta.url)
-
+const databasePath = new URL('../db.json', import.meta.url)
 export class Database {
-    #database = {}
-    constructor(){
-        fs.readFile(databasepath, "utf8").then(data =>{
-            this.#database = JSON.parse(data)
-        })
-        .catch(()=> {
-            this.#parsist()
-        })
+  #database = {}
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+  select(table) {
+    const data = this.#database[table] ?? []
+    return data
+  }
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
     }
-    #parsist(){
-        fs.writeFile('db.json',JSON.stringify(this.#database))
+    this.#persist()
+    return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
     }
+  }
 
-    select(table){
-        const data = this.#database[table]??[]
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
-        return data
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
     }
-
-    insert(table,data){
-        if (Array.isArray(this.#database[table])){
-            this.#database[table].push(data)
-        }else {
-            this.#database[table] = [data]
-        }
-        this.#parsist()
-
-        return data
-    }
+  }
 }
